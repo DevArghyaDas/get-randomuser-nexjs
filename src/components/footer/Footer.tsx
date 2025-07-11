77250fd50151ce0cab2272b338b39e993ae2f315
Arghya Das <mrarghyadasdev@gmail.com>
2025-07-11 17:15:15 +0530

    z index issu solved!

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-6 px-4 fixed bottom-0 border-t border-default-400 w-full flex justify-center backdrop-blur-md bg-default-100/60">
+    <footer className="py-6 px-4 fixed bottom-0 border-t border-default-400 w-full flex justify-center backdrop-blur-md bg-default-100/60 z-50">
       <span>
         &copy; {new Date().getFullYear()} RandomUser App. All rights reserved.
       </span>
